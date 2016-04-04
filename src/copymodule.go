6bae81e37257ec244f5b54ae5aa926a5c4059816
Diego Cena <diego.cena@gmail.com>
2016-04-04 18:25:53 -0300

    dev commit

--- src/copymodule.go
+++ src/copymodule.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	//	"bytes"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
 )
 
 type CopyModule TCamerataModule
@@ -15,42 +17,53 @@ func (me *CopyModule) Prepare(host string, sshconn *SshConnection) error {
 		return CamerataModuleError{"CopyModule: Arguments cannot be empty"}
 	}
 
+	commandargs := strings.Split(me.args.MArguments, "|")
+	if len(commandargs) != 2 {
+		return CamerataModuleError{"CopyModule: Arguments must be src_file|dest_dir"}
+	}
+
 	return nil
 }
 
 func (me *CopyModule) Run() error {
 
-	commandargs := me.args.MArguments
+	commandargs := strings.Split(me.args.MArguments, "|")
 
-	fmt.Print(">>> CopyModule >>> Copying ", commandargs, "@", me.host)
+	fmt.Print(">>> CopyModule >>> Copying ", me.args.MArguments, "@", me.host)
 	if me.args.Sudo {
 		fmt.Print(" as sudo")
 	}
 	fmt.Println("")
 
-	//	commandline := commandargs
+	session, err := me.sshconn.client.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
 
-	//	session, err := me.sshconn.client.NewSession()
-	//	if err != nil {
-	//		panic("Failed to create session: " + err.Error())
-	//	}
-	//	defer session.Close()
+	filename := filepath.Base(commandargs[0])
 
-	//	if me.args.Sudo {
-	//		commandline = fmt.Sprintf("sudo -S \"%s\"", commandargs)
+	fmt.Println(">>> Filename is", filename)
 
-	//		go func() {
-	//			w, _ := session.StdinPipe()
-	//			defer w.Close()
-	//			fmt.Fprintln(w, me.args.Pass)
-	//		}()
-	//	}
-	//	var b bytes.Buffer
-	//	session.Stdout = &b
-	//	if err := session.Run(commandline); err != nil {
-	//		panic("Failed to run: " + err.Error())
-	//	}
-	//	fmt.Println(">>> CopyModule >>>", b.String())
+	go func() {
+		w, _ := session.StdinPipe()
+		defer w.Close()
+
+		content, err := ioutil.ReadFile(commandargs[0])
+		if err != nil {
+			panic("Failed to read file " + commandargs[0])
+		}
+
+		fmt.Fprintln(w, "C0644", len(content), filename)
+		fmt.Fprint(w, content)
+		fmt.Fprint(w, "\x00")
+	}()
+
+	scp_command := fmt.Sprintf("scp -qrt %s/%s", commandargs[1], filename)
+
+	if err := session.Run(scp_command); err != nil {
+		panic("Failed to run: " + err.Error())
+	}
 
 	return nil
 
