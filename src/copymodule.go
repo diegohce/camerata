616cddbe03a95060d90cea295d141b3a2c802df0
Diego Cena <diego.cena@gmail.com>
2016-04-05 13:39:19 -0300

    dev commit

--- src/copymodule.go
+++ src/copymodule.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	//"bytes"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -42,28 +43,41 @@ func (me *CopyModule) Run() error {
 	defer session.Close()
 
 	filename := filepath.Base(commandargs[0])
+	fmt.Println(">>> CopyModule >>> Filename is", filename)
 
-	fmt.Println(">>> Filename is", filename)
+	fp, err := os.Open(commandargs[0])
+	if err != nil {
+		panic("Error opening file " + err.Error())
+	}
+	byte_buffer := make([]byte, 4096)
+	fileinfo, err := fp.Stat()
+	fmt.Println(">>> CopyModule >>> Filesize is", fileinfo.Size())
 
 	go func() {
 		w, _ := session.StdinPipe()
 		defer w.Close()
 
-		content, err := ioutil.ReadFile(commandargs[0])
-		if err != nil {
-			panic("Failed to read file " + commandargs[0])
-		}
+		fmt.Fprintln(w, "C0644", fileinfo.Size(), filename)
 
-		fmt.Fprintln(w, "C0644", len(content), filename)
-		fmt.Fprint(w, content)
+		count, _ := fp.Read(byte_buffer)
+		for count > 0 {
+			fmt.Println(">>> CopyModule >>> sending", count, "bytes")
+			fmt.Fprintf(w, "%s", byte_buffer[:count])
+			count, _ = fp.Read(byte_buffer)
+		}
 		fmt.Fprint(w, "\x00")
+
 	}()
 
 	scp_command := fmt.Sprintf("scp -qrt %s/%s", commandargs[1], filename)
+	//fmt.Println(">>> scp command is", scp_command)
 
+	//var b bytes.Buffer
+	//session.Stdout = &b
 	if err := session.Run(scp_command); err != nil {
 		panic("Failed to run: " + err.Error())
 	}
+	//fmt.Println(">>> CopyModule >>>", b.String())
 
 	return nil
 
